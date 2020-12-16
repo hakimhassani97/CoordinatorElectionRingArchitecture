1982488af2fda17ce6c935761d7258f1451e8183
hakimhassani97 <hakimhassani97@gmail.com>
2020-12-16 23:54:34 +0100

    updated p1

--- p1.py
+++ p1.py
@@ -1,29 +1,83 @@
 import socket
 import config
+import threading
+import random
+import time
+import json
 
 # parametres locals
 numProcess = 1
-localPort = config.BASE_PORT + numProcess
+id = random.randint(1, 100)
 pSuiv = (numProcess + 1) % config.NB_PROCESSUS
-pPrec = (numProcess - 1) % config.NB_PROCESSUS
-# debut
-msgToSend = "Hello UDP Client"+str(numProcess)
-bytesToSend = str.encode(msgToSend)
+pPrec = config.NB_PROCESSUS if numProcess==1 else (numProcess-1)
+sendPort = config.BASE_PORT + (0 if numProcess==config.NB_PROCESSUS else (numProcess))
+recvPort = config.BASE_PORT + (0 if numProcess==1 else (numProcess-1))
 
-# creer une socket datagrame
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# Bind to address and ip
-UDPServerSocket.bind((config.HOST, localPort))
-print("UDP server up and listening")
+# liste des processus presents sous forme [(id, @),]
+listeProcessus = []
+leader = None
 
-# reception des datagrames
-while(True):
-    bytesAddressPair = UDPServerSocket.recvfrom(config.BUFFER_SIZE)
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    print(clientMsg)
-    print(clientIP)
-    # envoi d'une reponse
-    UDPServerSocket.sendto(bytesToSend, address)
+# creer une socket datagrame de reception
+UDPRecvSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# UDPRecvSocket.setblocking(0)
+# relier la socket a l@
+addressPortRecepteur = (config.HOST, recvPort)
+UDPRecvSocket.bind(addressPortRecepteur)
+print("serveur UDP en attente sur {}".format(addressPortRecepteur))
+
+# creer une socket datagrame d'envoi
+UDPSendSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+class RecvThread(threading.Thread):
+    def __init__(self):
+        super(RecvThread, self).__init__()
+
+    def run(self):
+        # reception des datagrames
+        while(True):
+            # try:
+            bytesAddressPair = UDPRecvSocket.recvfrom(config.BUFFER_SIZE)
+            data = bytesAddressPair[0]
+            temp = json.loads(data)['temp']
+            senderId = json.loads(data)['id']
+            address = bytesAddressPair[1]
+            print('recu {} par {}'.format(temp, senderId))
+            # enregistrer le processus s'il n'existe pas
+            if (senderId, address) not in listeProcessus:
+                listeProcessus.append((senderId, address))
+                print('liste des processus :', listeProcessus)
+            global leader
+            if leader==None and len(listeProcessus)==config.NB_PROCESSUS:
+                leader = max(listeProcessus, key=lambda x: x[0])
+            # except:
+            #     print('erreur de reception')
+
+class SendThread(threading.Thread):
+    def __init__(self):
+        super(SendThread, self).__init__()
+
+    def run(self):
+        # envoi des datagrames
+        addressPortEnvoyeur = (config.HOST, sendPort)
+        i = 5
+        while(i>0):
+            i -= 1
+            time.sleep(5)
+            temp = random.randint(0, 40)
+            # temp = str(temp)
+            data = json.dumps({'temp':temp, 'id':id, 'to':leader[0] if leader!=None else None})
+            tempBytes = str.encode(data)
+            # envoyer la temperature
+            try:
+                UDPSendSocket.sendto(tempBytes, addressPortEnvoyeur)
+                print('processus {} envoi {} a {}'.format(numProcess, temp, addressPortEnvoyeur))
+            except:
+                print('erreur envoi {} vers {}'.format(numProcess, addressPortEnvoyeur))
+
+recvT = RecvThread()
+sendT = SendThread()
+recvT.start()
+sendT.start()
+
+recvT.join()
+sendT.join()
